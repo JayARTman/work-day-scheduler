a1156c1623103a7e2412dc3268db18a850487d0b
jason@jhmedia.tv <jasonhartman@jessicas-mbp.home>
2021-05-16 08:42:54 -0500

    Javascript Update

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 $(".saveBtn").click(function(){
     let textValue = $(this).siblings(".description").val()
     console.log(textValue);
+    let id = $(this).parent().attr("id")
+    console.log(id)
+    localStorage.setItem(id,textValue)
 })
+
+function getSavedItems(){
+    let currentHour = moment().hours()
+    console.log(currentHour);
+    $(".time-block").each(function(){
+        console.log("testing");
+        let id = $(this).attr("id")
+        $(this).find($(".description")).val(localStorage.getItem(id))
+        if (currentHour > id){
+            $(this).addClass("past")
+        }
+        else if (currentHour === id){
+            $(this).addClass("present")
+        }
+        else {
+            $(this).addClass("future")
+        }
+        
+    })
+}
+
+getSavedItems()
